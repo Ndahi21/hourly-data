e8fd93ecc7b7c8e92acbdb0c66b674a77e23d34f
Israel Trejo <isrtrejo07@gmail.com>
2026-05-29 21:05:44 -0500

    Rearranged subjects boxes and saving subject types

--- client/components/SideBar.tsx
+++ client/components/SideBar.tsx
@@ -3,38 +3,26 @@ import { Menu } from 'lucide-react';
 
 export default function SideBar() {
 
+  const subjects = [
+    { name: "Sleep", color: "#535353" },
+    { name: "Social", color: "#aef6ff" },
+    { name: "Work", color: "#926828" },
+    { name: "Art", color: "#ff0000" },
+    { name: "University", color: "#7011be" },
+    { name: "Computer Science", color: "#2a05ff" },
+    { name: "Exercise", color: "#12d401" },
+  ]
+
   return (
-    <div className="fixed p-[20px] ml-[0px] w-[280px] h-[100%] font-sans bg-white z-[1000]">
+    <div className="fixed p-[20px] pt-[10px] ml-[0px] w-[280px] h-[100%] font-sans bg-white z-[1000]">
       <div className="ml-[52px]">
-        <h3 className="mb-[10px] font-bold text-lg">Subjects:</h3>
-        <div className="flex flex-row items-center pb-[6px]">
-          <div className="w-[18px] h-[18px] border-[3px] border-black bg-[#535353]"></div>
-          <span className="ml-[12px] text-[18px]">Sleep</span>
-        </div>
-        <div className="flex flex-row items-center pb-[6px]">
-          <div className="w-[18px] h-[18px] border-[3px] border-black bg-[#aef6ff]"></div>
-          <span className="ml-[12px] text-[18px]">Social</span>
-        </div>
-        <div className="flex flex-row items-center pb-[6px]">
-          <div className="w-[18px] h-[18px] border-[3px] border-black bg-[#4444a5]"></div>
-          <span className="ml-[12px] text-[18px]">University</span>
-        </div>
-        <div className="flex flex-row items-center pb-[6px]">
-          <div className="w-[18px] h-[18px] border-[3px] border-black bg-[#2a05ff]"></div>
-          <span className="ml-[12px] text-[18px]">Computer Science</span>
-        </div>
-        <div className="flex flex-row items-center pb-[6px]">
-          <div className="w-[18px] h-[18px] border-[3px] border-black bg-[#fc0000]"></div>
-          <span className="ml-[12px] text-[18px]">Art</span>
-        </div>
-        <div className="flex flex-row items-center pb-[6px]">
-          <div className="w-[18px] h-[18px] border-[3px] border-black bg-[#14db02]"></div>
-          <span className="ml-[12px] text-[18px]">Exercise</span>
-        </div>
-        <div className="flex flex-row items-center pb-[6px]">
-          <div className="w-[18px] h-[18px] border-[3px] border-black bg-[#a97d3b]"></div>
-          <span className="ml-[12px] text-[18px]">Organization</span>
-        </div>
+        <h3 className="mb-[6px] font-bold text-lg">Subjects:</h3>
+        {subjects.map((subject, index) => (
+          <div key={index} className="flex flex-row items-center gap-[10px] mb-[4px]">
+            <div style={{ backgroundColor: subject.color }} className="w-[16px] h-[16px] border-[2px] border-black"></div>
+            <p className="text-[16px] font-semibold">{subject.name}</p>
+          </div>
+        ))}
       </div>
 
       <div className="ml-[20px]">
