c659d8870338a3fc9c98377bb01607ea8c472ce8
Israel Trejo <isrtrejo07@gmail.com>
2026-08-16 23:14:35 -0500

    survival logbook

---
+++ client/components/ReviewDay.tsx
@@ -0,0 +1,5 @@
+const dayWas = ['Happy', 'Meh', 'Sad']
+const dayMood = ['Energetic', 'Neutral', 'Tired']
+
+const food = ['Coffee', 'Sugar', 'Fast Food']
+const activity = ['Work', 'Relaxation', 'Flea Market', 'Socializing', 'Exercise', 'Gaming', 'Reading', 'Cooking', 'Shopping', 'Traveling']
